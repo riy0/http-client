4d4474ecc8cd54f491c74c3a0b2ced55fd494340
riy0 <chac0ptx@gmail.com>
2020-05-24 15:42:23 +0900

    [update] testのために構成変更. mainから直接method操作

--- client/src/main.rb
+++ client/src/main.rb
@@ -2,32 +2,24 @@ require 'net/http'
 
 # Method: get, post.
 class HttpClient
+  attr_reader :method
+
   def initialize(input)
     @url = input[0]
     @method = input[1]
     @parameter = format_parameter(input[2])
-    @thread_number = input[3]
+    @thread_number = input[3].to_i
     @times = input[4]
     @response = input[5]
-  end
-
-  def execute
-    case @method
-    when 'get', 'GET', 'g'
-      request
-    else
-      puts 'No method'
-      usage
-    end
   end
 
   # get
   def request
     uri = URI.parse(@url)
     uri.query = URI.encode_www_form(@parameter) if @parameter.is_a?(Hash)
-    res = Net::HTTP.get_response(uri)
 
-    show_response(res)
+    res = Net::HTTP.get_response(uri)
+    puts res.code
   end
 
   private
@@ -48,15 +40,10 @@ class HttpClient
   end
 
   def show_response(result)
-    result.body if @response == 'body'
+    puts result.body if @response == 'body'
 
     # [todo] correspond to show the number of each status code
-    result.code if @response == 'status'
-  end
-
-  # [todo] validation method
-  def validate_arguments
-    puts 'validate input'
+    puts result.code if @response == 'status'
   end
 end
 
@@ -64,17 +51,30 @@ def usage
   print 'コマンドライン引数が正しくありません'
 end
 
+# [todo] validation method
+def validate_arguments
+  return unless ARGV.size == 6
+
+  'validate input'
+end
+
 def main
-  if ARGV.size != 6
+  if validate_arguments.nil?
     usage
     exit
   end
 
   client = HttpClient.new(ARGV)
-  client.execute
+
+  case client.method
+  when 'get', 'GET', 'g'
+    client.request
+  else
+    usage
+    exit
+  end
 end
 
 if __FILE__ == $0
   main
 end
-
