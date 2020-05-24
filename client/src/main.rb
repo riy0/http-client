b221f2646b2de8bf9953a154c4340d3980fb9a36
riy0 <chac0ptx@gmail.com>
2020-05-24 16:55:15 +0900

    [update] mainの位置修正

--- client/src/main.rb
+++ client/src/main.rb
@@ -2,24 +2,32 @@ require 'net/http'
 
 # Method: get, post.
 class HttpClient
-  attr_reader :method
-
   def initialize(input)
     @url = input[0]
     @method = input[1]
     @parameter = format_parameter(input[2])
-    @thread_number = input[3].to_i
+    @thread_number = input[3]
     @times = input[4]
     @response = input[5]
+  end
+
+  def execute
+    case @method
+    when 'get', 'GET', 'g'
+      request
+    else
+      puts 'No method'
+      usage
+    end
   end
 
   # get
   def request
     uri = URI.parse(@url)
     uri.query = URI.encode_www_form(@parameter) if @parameter.is_a?(Hash)
-
     res = Net::HTTP.get_response(uri)
-    puts res.code
+
+    show_response(res)
   end
 
   private
@@ -40,10 +48,15 @@ class HttpClient
   end
 
   def show_response(result)
-    puts result.body if @response == 'body'
+    result.body if @response == 'body'
 
     # [todo] correspond to show the number of each status code
-    puts result.code if @response == 'status'
+    result.code if @response == 'status'
+  end
+
+  # [todo] validation method
+  def validate_arguments
+    puts 'validate input'
   end
 end
 
@@ -51,30 +64,12 @@ def usage
   print 'コマンドライン引数が正しくありません'
 end
 
-# [todo] validation method
-def validate_arguments
-  return unless ARGV.size == 6
-
-  'validate input'
-end
-
-def main
-  if validate_arguments.nil?
+if __FILE__ == $0
+  if ARGV.size != 6
     usage
     exit
   end
 
   client = HttpClient.new(ARGV)
-
-  case client.method
-  when 'get', 'GET', 'g'
-    client.request
-  else
-    usage
-    exit
-  end
-end
-
-if __FILE__ == $0
-  main
+  client.execute
 end
