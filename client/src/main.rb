41244b9834de03d6b9b319840c5fac20987a5f5b
riy0 <chac0ptx@gmail.com>
2020-05-25 20:55:29 +0900

    [fix] 初期化, 名前

--- client/src/main.rb
+++ client/src/main.rb
@@ -1,31 +1,39 @@
 require 'net/http'
 
 # Method: get, post.
-class HttpClient
+class HTTPClient
   attr_reader :method
 
-  def initialize(input)
-    @url = input[0]
-    @method = input[1]
-    @parameter = format_parameter(input[2])
-    @thread_number = input[3]
-    @times = input[4]
-    @response = input[5]
+  def initialize(url, method, parameter, thread_number, repeat_count, response)
+    @url = url
+    @method = method
+    @parameter = format_parameter_to_hash(parameter)
+    @thread_number = thread_number.to_i
+    @repeat_count = repeat_count.to_i
+    @response = response
+  end
+
+  def execute
+    case @method
+    when 'get', 'GET', 'g'
+      http_get_request
+    else
+      my_exit
+    end
   end
 
   # get
-  def request
+  def http_get_request
     uri = URI.parse(@url)
     uri.query = URI.encode_www_form(@parameter) if @parameter.is_a?(Hash)
 
-    results = parallelize_request(uri)
+    results = parallelize_requests(uri)
     display_results(results)
   end
 
   private
 
-  # format parameter to hash class.
-  def format_parameter(parameters)
+  def format_parameter_to_hash(parameters)
     return unless parameters.include?('=')
 
     formatted_params = {}
@@ -39,39 +47,39 @@ class HttpClient
     formatted_params
   end
 
-  def parallelize_request(uri)
+  def parallelize_requests(uri)
     threads = []
-    results = []
+    responses = []
     count = 0
 
-    until count == @times 
+    until count == @repeat_count
       @thread_number.times do 
-        break if count == @times
+        break if count == @repeat_count
 
         threads << Thread.new do
           res = Net::HTTP.get_response(uri)
-          results << res if @response == 'body'
-          results << res.code if @response == 'status'
+          responses << res if @response == 'body'
+          responses << res.code if @response == 'status'
         end
         count += 1
       end
 
       threads.each(&:join)
     end
-    results
+    responses
   end
 
   def display_results(results)
     # puts "run #{@method} request"
     # puts "url: #{@url}"
-    # puts "thread: #{@thread_number}, reputation: #{@times}"
+    # puts "thread: #{@thread_number}, iteration: #{@iteration_count} times"
 
-    show_response_body(results) if @response == 'body'
-    show_total_status(results) if @response == 'status'
+    check_response_body(results) if @response == 'body'
+    puts count_each_status(results) if @response == 'status'
   end
 
   # if get response body, return it
-  def show_response_body(results)
+  def check_response_body(results)
     results.each do |result|
       next unless result.is_a?(Net::HTTPSuccess)
       next if result.body == '[]'
@@ -82,7 +90,7 @@ class HttpClient
     'unmatched parameter value'
   end
 
-  def show_total_status(results)
+  def count_each_status(results)
     messages = []
     status_codes = results.uniq
 
@@ -101,26 +109,17 @@ def my_exit
   exit
 end
 
-def validate_arguments
-  return unless ARGV.size == 6
-  return unless ARGV[5] == 'body' || ARGV[5] == 'status'
-
-  thread_number = ARGV[3].to_i
-  times = ARGV[4].to_i
-  thread_number = times if thread_number > times
-  ARGV[3] = thread_number
-  ARGV[4] = times
-end
-
 if __FILE__ == $0
-  my_exit if validate_arguments.nil?
+  my_exit unless ARGV.size == 6
+  my_exit unless ARGV[5] == 'body' || ARGV[5] == 'status'
 
-  client = HttpClient.new(ARGV)
+  url = ARGV[0]
+  method = ARGV[1]
+  parameter = ARGV[2]
+  thread_number = ARGV[3]
+  repeat_count = ARGV[4]
+  response = ARGV[5]
 
-  case client.method
-  when 'get', 'GET', 'g'
-    client.request
-  else
-    my_exit
-  end
+  client = HTTPClient.new(url, method, parameter, thread_number, repeat_count, response)
+  client.execute
 end
