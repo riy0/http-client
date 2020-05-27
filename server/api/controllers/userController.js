3b7389066812e309401e40ffab428e16818bb5a7
riy0 <chac0ptx@gmail.com>
2020-05-27 21:32:56 +0900

    [add] controller

---
+++ server/api/controllers/userController.js
@@ -0,0 +1,53 @@
+var mongoose = require("mongoose"),
+  User = mongoose.model("Users");
+
+// get all users
+exports.all_users = function(req, res) {
+  User.find({}, function(err, user) {
+    if (err) res.send(err);
+    res.json(user);
+  });
+};
+
+// add new user
+exports.create_user = function(req, res) {
+  var new_user = new User(req.body);
+  new_user.save(function(err, user) {
+    if (err) res.send(err);
+    res.json(user);
+  });
+};
+
+// get a user 
+exports.load_user = function(req, res) {
+  User.findById(req.params.userId, function(err, user) {
+    if (err) res.send(err);
+    res.json(user);
+  });
+};
+
+// update user 
+exports.update_user = function(req, res) {
+  User.findOneAndUpdate(
+    { _id: req.params.userId },
+    req.body,
+    { new: true },
+    function(err, user) {
+      if (err) res.send(err);
+      res.json(user);
+    }
+  );
+};
+
+// delete user
+exports.delete_user = function(req, res) {
+  User.remove(
+    {
+      _id: req.params.userId
+    },
+    function(err, user) {
+      if (err) res.send(err);
+      res.json({ message: "User successfully deleted" });
+    }
+  );
+};
